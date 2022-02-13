9512c68b8ca0f2182eafb38f152a6677137369f8
JohnLazz <johnlazzari1@gmail.com>
2022-02-13 20:11:39 +0000

    Add js to flash squares

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,8 @@ let currentGameSeq = [];
 let currentScore = $(".current-score");
 let highScore = $(".high-score");
 
-// Gets random sqaure from squares array using math.random on array length. 
+
+// Gets random square from squares array using math.random on array length. 
 function randomSquare() {
     let r = Math.floor(Math.random() * squares.length);
     console.log(squares[r]);
@@ -11,4 +12,25 @@ function randomSquare() {
   }
 
 randomSquare();
+
+// Adds then removes opaque css class to square using timeout inside promise, to create flashing effect.
+function flashSquare(square) {
+    return new Promise((resolve) => {
+        square.classList.add("opaque");
+        setTimeout(() => {
+            resolve(square.classList.remove("opaque"));
+        }, 1000);
+    });
+};
+
+// Testing flashSquare on squares array using async function to await each flash. 
+const flashing = async () => {
+    for (let square of squares) {
+        await flashSquare(square);
+    }
+};
+
+flashing();
+
+
 
