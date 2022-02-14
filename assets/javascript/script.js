7b72733bf9a622294bdc6004a5ecd9268afa9fcf
JohnLazz <johnlazzari1@gmail.com>
2022-02-14 16:17:58 +0000

    Add 2nd setTimeout to flashSquare function for better usability

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,36 +1,44 @@
 const squares = document.querySelectorAll(".game-square");
 let currentGameSeq = [];
-let currentScore = $(".current-score");
-let highScore = $(".high-score");
+let userSeq = [];
+let currentScore = document.querySelector(".current-score").innerHTML;
+let highScore = document.querySelector(".high-score").innerHTML;
 
 
 // Gets random square from squares array using math.random on array length. 
 function randomSquare() {
     let r = Math.floor(Math.random() * squares.length);
-    console.log(squares[r]);
     return squares[r];
   }
 
 randomSquare();
+
+// Pushes a random square aquired from randomSquare into currentGameSeq array.
+function gameFunction() {
+    currentGameSeq.push(randomSquare());
+    console.log(currentGameSeq);
+}
 
 // Adds then removes opaque css class to square using timeout inside promise, to create flashing effect.
 function flashSquare(square) {
     return new Promise((resolve) => {
         square.classList.add("opaque");
         setTimeout(() => {
-            resolve(square.classList.remove("opaque"));
+            (square.classList.remove("opaque"));
+            setTimeout(() => {
+                resolve();
+            }, 500);
         }, 1000);
     });
 };
 
-// Testing flashSquare on squares array using async function to await each flash. 
-const flashing = async () => {
-    for (let square of squares) {
+// Indicates game sequence by calling flashSquare on currentGameSeq, array using async function to await each flash. 
+async function flashing() {
+    for (let square of currentGameSeq) {
         await flashSquare(square);
     }
-};
+}
 
-flashing();
 
 
 
