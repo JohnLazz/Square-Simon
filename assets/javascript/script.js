26885928d5f250ec2f3789e3ab5c69ecf3d9e06d
JohnLazz <johnlazzari1@gmail.com>
2022-02-14 18:12:41 +0000

    Add function to check if user/game sequence arrays are equal

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,15 +1,32 @@
 const squares = document.querySelectorAll(".game-square");
+const firstSquare = document.querySelector(".square-1");
+const secondSquare = document.querySelector(".square-2");
+const thirdSquare = document.querySelector(".square-3");
+const fourthSquare = document.querySelector(".square-4");
+const startButton = document.querySelector(".start-game");
 let currentGameSeq = [];
 let userSeq = [];
 let currentScore = document.querySelector(".current-score").innerHTML;
 let highScore = document.querySelector(".high-score").innerHTML;
 
+$(".square-1").click(function() {
+    userSeq.push(firstSquare);
+});
+$(".square-2").click(function() {
+    userSeq.push(secondSquare);
+});
+$(".square-3").click(function() {
+    userSeq.push(thirdSquare);
+});
+$(".square-4").click(function() {
+    userSeq.push(fourthSquare);
+});
 
 // Gets random square from squares array using math.random on array length. 
 function randomSquare() {
     let r = Math.floor(Math.random() * squares.length);
     return squares[r];
-  }
+  };
 
 randomSquare();
 
@@ -17,7 +34,7 @@ randomSquare();
 function gameFunction() {
     currentGameSeq.push(randomSquare());
     console.log(currentGameSeq);
-}
+};
 
 // Adds then removes opaque css class to square using timeout inside promise, to create flashing effect.
 function flashSquare(square) {
@@ -37,8 +54,17 @@ async function flashing() {
     for (let square of currentGameSeq) {
         await flashSquare(square);
     }
-}
+};
 
-
-
-
+// Checks if arrays are equal. Without this userSeq == currentGameSeq returns false even with the same values.
+// Code taken from: https://stackoverflow.com/questions/3115982/how-to-check-if-two-arrays-are-equal-with-javascript
+function arraysEqual(a, b) {
+    if (a === b) return true;
+    if (a == null || b == null) return false;
+    if (a.length !== b.length) return false;
+  
+    for (var i = 0; i < a.length; ++i) {
+      if (a[i] !== b[i]) return false;
+    }
+    return true;
+  }
